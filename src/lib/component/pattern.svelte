35e56659443e3a6b37ca7e9f56c09bca7d1e1946
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-11-27 19:43:51 +0330

    feat: build Pattern component

---
+++ src/lib/component/pattern.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	type PatternProps = {
+		class?: string;
+		visibility?: 'low' | 'high';
+	};
+	const { class: className, visibility = 'low' }: PatternProps = $props();
+
+	const visibilityMap = {
+		low: 'visibility-low',
+		high: 'visibility-high'
+	};
+</script>
+
+<img
+	aria-hidden="true"
+	src="/image/pattern.svg"
+	alt="Background pattern with lines similar to a circuit board"
+	class={['pattern', visibilityMap[visibility], className].join(' ')}
+/>
+
+<style>
+	.pattern {
+		pointer-events: none;
+		width: 320px;
+	}
+
+	.visibility-low {
+		opacity: 0.03;
+	}
+
+	.visibility-high {
+		opacity: 0.05;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
+	import Pattern from '$lib/component/pattern.svelte';
 	import '$lib/style/main.css';
 	import Header from './header.svelte';
 	const { children } = $props();
 </script>
 
 <div class="container">
+	<Pattern class="background-pattern" />
 	<Header />
 	{@render children()}
 </div>
@@ -14,5 +16,14 @@
 		max-width: 128rem;
 		margin-inline: auto;
 		padding: 3.2rem;
+	}
+
+	.container :global(.background-pattern) {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		min-width: 1920px;
+		z-index: -1;
 	}
 </style>
