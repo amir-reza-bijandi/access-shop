ece05271b2e5ec3a70cc25fbc22283f1d69d0a85
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-11-29 14:38:57 +0330

    refactor: move background patterns to a separate component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,27 @@
 <script>
 	import Pattern from '$lib/component/pattern.svelte';
 	import '$lib/style/main.css';
+	import Background from './background.svelte';
 	import Header from './header.svelte';
 	const { children } = $props();
 </script>
 
-<div class="container">
-	<Pattern class="top-pattern" />
-	<Header />
-	{@render children()}
-	<Pattern class="bottom-pattern" />
+<div class="app">
+	<Background />
+	<div class="container">
+		<Header />
+		{@render children()}
+	</div>
 </div>
 
 <style>
-	.container {
+	.app {
 		position: relative;
+	}
+
+	.container {
 		max-width: 128rem;
 		margin-inline: auto;
 		padding: 3.2rem;
-	}
-
-	.container :global(.top-pattern),
-	.container :global(.bottom-pattern) {
-		position: absolute;
-		left: 50%;
-		transform: translateX(-50%);
-		min-width: 1920px;
-		z-index: -1;
-	}
-
-	.container :global(.top-pattern) {
-		top: 0;
-	}
-
-	.container :global(.bottom-pattern) {
-		bottom: 0;
-		transform: translateX(-50%) rotate(180deg);
 	}
 </style>
---
+++ src/routes/background.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Pattern from '$lib/component/pattern.svelte';
+</script>
+
+<div class="background">
+	<Pattern />
+	<Pattern />
+</div>
+
+<style>
+	.background {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		height: 100%;
+		z-index: -1;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.background :global(> *) {
+		width: 192rem;
+		height: 97.4rem;
+	}
+
+	.background :global(:last-of-type) {
+		margin-top: auto;
+		transform: rotate(180deg);
+	}
+</style>
