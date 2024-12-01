c011c578705d54e14f153224b12d35ec66283a2a
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-01 12:33:54 +0330

    feat: add dynamic positon option to ripple effect

--- src/lib/action/ripple-effect.svelte.ts
+++ src/lib/action/ripple-effect.svelte.ts
@@ -1,24 +1,39 @@
 import type { Action } from 'svelte/action';
 
-const rippleEffect: Action<HTMLElement> = (node) => {
-	node.classList.add('ripple-container');
+type Params = {
+	// Ripple position is based on the current mouse position
+	dynamicPositon?: boolean;
+};
+
+const rippleEffect: Action<HTMLElement, Params | undefined> = (node, params) => {
 	function handleClick(e: MouseEvent) {
-		const duration = parseInt(
-			getComputedStyle(document.documentElement).getPropertyValue('--ripple-duration')
-		);
-		const ripple = document.createElement('span');
-		ripple.classList.add('ripple');
-		ripple.style.width = `${node.getBoundingClientRect().width}px`;
-		ripple.style.left = `${e.clientX - node.getBoundingClientRect().left}px`;
-		ripple.style.top = `${e.clientY - node.getBoundingClientRect().top}px`;
-		node.appendChild(ripple);
-		setTimeout(() => {
-			node.removeChild(ripple);
-		}, duration);
+		if (e.button === 0) {
+			const { dynamicPositon = true } = params || {};
+			node.classList.add('ripple-container');
+			const nodeRect = node.getBoundingClientRect();
+
+			const duration = parseInt(getComputedStyle(node).getPropertyValue('--ripple-duration'));
+			const ripple = document.createElement('span');
+			ripple.classList.add('ripple');
+			ripple.style.width = `${nodeRect.width}px`;
+
+			if (dynamicPositon) {
+				ripple.style.left = `${e.clientX - nodeRect.left}px`;
+				ripple.style.top = `${e.clientY - nodeRect.top}px`;
+			} else {
+				ripple.style.left = `${nodeRect.width / 2}px`;
+				ripple.style.top = `${nodeRect.height / 2}px`;
+			}
+
+			node.appendChild(ripple);
+			setTimeout(() => {
+				node.removeChild(ripple);
+			}, duration);
+		}
 	}
 	$effect(() => {
-		node.addEventListener('click', handleClick);
-		return () => node.removeEventListener('click', handleClick);
+		node.addEventListener('mousedown', handleClick);
+		return () => node.removeEventListener('mousedown', handleClick);
 	});
 };
 
