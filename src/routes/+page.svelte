2b7129f8c1b0ff886fcaa19e970ca3b9046b726f
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-03 13:06:47 +0330

    refactor: ProductGrid on home page to a separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
 	import Faq from './_lib/component/faq.svelte';
 	import Hero from './_lib/component/hero.svelte';
-	import ProductGrid from '../lib/component/product-grid.svelte';
+	import Products from './_lib/component/products.svelte';
 	import Testimonial from './_lib/component/testimonial.svelte';
-	import { productInfoList } from '$lib/data/products';
 </script>
 
 <Hero />
 <div class="wrapper">
-	<ProductGrid
-		infoList={productInfoList}
-		title={{
-			text: 'محصولات اکسس شاپ',
-			description: 'سفرت تو دنیای هوش مصنوعی از اینجا شروع می‌شه!'
-		}}
-	/>
+	<Products />
 	<Testimonial />
 	<Faq />
 </div>
---
+++ src/routes/_lib/component/products.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ProductGrid from '$lib/component/product-grid.svelte';
+	import { productInfoList } from '$lib/data/products';
+</script>
+
+<ProductGrid
+	infoList={productInfoList}
+	title={{
+		text: 'محصولات اکسس شاپ',
+		description: 'سفرت تو دنیای هوش مصنوعی از اینجا شروع می‌شه!'
+	}}
+/>
