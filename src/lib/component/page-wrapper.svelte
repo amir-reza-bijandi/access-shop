8ab92212e025278f12df09c95d6f9365e55daa3e
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-03 13:34:40 +0330

    feat: create PageWrapper component to handle page padding

---
+++ src/lib/component/page-wrapper.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type PageWrapperProps = {
+		children: Snippet;
+	};
+	const { children }: PageWrapperProps = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	main {
+		padding-block: 6.4rem;
+	}
+
+	@media (max-width: 72rem) {
+		main {
+			padding-block: 4.8rem;
+		}
+	}
+
+	@media (max-width: 32rem) {
+		main {
+			/* Using a higher value to account for navigation on mobile */
+			padding-top: 3.2rem;
+			padding-bottom: 6.8rem;
+		}
+	}
+</style>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,28 +1,9 @@
 <script lang="ts">
+	import PageWrapper from '$lib/component/page-wrapper.svelte';
 	import ProductGrid from '$lib/component/product-grid.svelte';
 	import { productInfoList } from '$lib/data/products';
 </script>
 
-<main>
+<PageWrapper>
 	<ProductGrid infoList={productInfoList} />
-</main>
-
-<style>
-	main {
-		padding-block: 6.4rem;
-	}
-
-	@media (max-width: 72rem) {
-		main {
-			padding-block: 4.8rem;
-		}
-	}
-
-	@media (max-width: 32rem) {
-		main {
-			/* Using a higher value to account for navigation on mobile */
-			padding-top: 3.2rem;
-			padding-bottom: 6.8rem;
-		}
-	}
-</style>
+</PageWrapper>
