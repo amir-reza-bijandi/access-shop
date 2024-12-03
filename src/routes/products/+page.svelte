c1206339dcd0576c4890cf8b72711ad8bb3aeddd
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-03 13:25:41 +0330

    feat: build Prodcuts page

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+	import ProductGrid from '$lib/component/product-grid.svelte';
+	import { productInfoList } from '$lib/data/products';
+</script>
+
 <main>
-	<h1>محصولات</h1>
+	<ProductGrid infoList={productInfoList} />
 </main>
+
+<style>
+	main {
+		padding-block: 6.4rem;
+	}
+
+	@media (max-width: 72rem) {
+		main {
+			padding-block: 4.8rem;
+		}
+	}
+
+	@media (max-width: 32rem) {
+		main {
+			/* Using a higher value to account for navigation on mobile */
+			padding-top: 3.2rem;
+			padding-bottom: 6.8rem;
+		}
+	}
+</style>
