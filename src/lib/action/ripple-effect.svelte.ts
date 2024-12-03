de8dbe7ece8839e697fab5303c913a32ab7610ec
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-03 12:09:04 +0330

    feat: extend ripple effect to keyboard interactions

--- src/lib/action/ripple-effect.svelte.ts
+++ src/lib/action/ripple-effect.svelte.ts
@@ -6,34 +6,48 @@ type Params = {
 };
 
 const rippleEffect: Action<HTMLElement, Params | undefined> = (node, params) => {
-	function handleClick(e: MouseEvent) {
-		if (e.button === 0) {
-			const { dynamicPositon = true } = params || {};
-			node.classList.add('ripple-container');
-			const nodeRect = node.getBoundingClientRect();
+	function createRipple(clientX?: number, clientY?: number) {
+		const { dynamicPositon = true } = params || {};
+		node.classList.add('ripple-container');
+		const nodeRect = node.getBoundingClientRect();
 
-			const duration = parseInt(getComputedStyle(node).getPropertyValue('--ripple-duration'));
-			const ripple = document.createElement('span');
-			ripple.classList.add('ripple');
-			ripple.style.width = `${nodeRect.width}px`;
+		const duration = parseInt(getComputedStyle(node).getPropertyValue('--ripple-duration'));
+		const ripple = document.createElement('span');
+		ripple.classList.add('ripple');
+		ripple.style.width = `${nodeRect.width}px`;
 
-			if (dynamicPositon) {
-				ripple.style.left = `${e.clientX - nodeRect.left}px`;
-				ripple.style.top = `${e.clientY - nodeRect.top}px`;
-			} else {
-				ripple.style.left = `${nodeRect.width / 2}px`;
-				ripple.style.top = `${nodeRect.height / 2}px`;
-			}
+		if (dynamicPositon && clientX && clientY) {
+			ripple.style.left = `${clientX - nodeRect.left}px`;
+			ripple.style.top = `${clientY - nodeRect.top}px`;
+		} else {
+			ripple.style.left = `${nodeRect.width / 2}px`;
+			ripple.style.top = `${nodeRect.height / 2}px`;
+		}
 
-			node.appendChild(ripple);
-			setTimeout(() => {
-				node.removeChild(ripple);
-			}, duration);
+		node.appendChild(ripple);
+		setTimeout(() => {
+			node.removeChild(ripple);
+		}, duration);
+	}
+
+	function handleClick({ clientX, clientY, button }: MouseEvent) {
+		if (button === 0) {
+			createRipple(clientX, clientY);
+		}
+	}
+
+	function handleKeyDown({ key }: KeyboardEvent) {
+		if (key === ' ' || key === 'Enter') {
+			createRipple();
 		}
 	}
 	$effect(() => {
 		node.addEventListener('mousedown', handleClick);
-		return () => node.removeEventListener('mousedown', handleClick);
+		node.addEventListener('keydown', handleKeyDown);
+		return () => {
+			node.removeEventListener('mousedown', handleClick);
+			node.removeEventListener('keydown', handleKeyDown);
+		};
 	});
 };
 
