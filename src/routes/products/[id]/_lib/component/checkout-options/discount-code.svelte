e9ad71a47fc23235c753cb7634c0a0970bec5daf
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2024-12-06 12:33:36 +0330

    feat: use Status component in comments and discount code sections

--- src/routes/products/[id]/_lib/component/checkout-options/discount-code.svelte
+++ src/routes/products/[id]/_lib/component/checkout-options/discount-code.svelte
@@ -5,21 +5,18 @@
 	import type { CheckoutContext } from '../../type/checkout';
 	import SwitchBoxInput from '../switch-box-input.svelte';
 	import type { FormEventHandler } from 'svelte/elements';
+	import Status from '$lib/component/status.svelte';
 
 	type Form = {
-		status: {
-			ok: boolean | null;
-			msg: string | null;
-		};
+		status: 'error' | 'success' | null;
+		message: string | null;
 		isSubmitting: boolean;
 	};
 
 	const context: CheckoutContext = getContext('checkout');
 	const form: Form = $state({
-		status: {
-			ok: null,
-			msg: null
-		},
+		status: null,
+		message: null,
 		isSubmitting: false
 	});
 
@@ -29,47 +26,37 @@
 		form.isSubmitting = true;
 		await new Promise((resolve) => setTimeout(resolve, 1000));
 		context.coupon = 200_000;
-		form.status = {
-			msg: 'کد تخفیف با موفقیت اعمال شد',
-			ok: true
-		};
+		form.status = 'success';
+		form.message = 'کد تخفیف با موفقیت اعمال شد';
 		form.isSubmitting = false;
 	};
 
 	const handleInput = () => {
-		form.status = {
-			msg: null,
-			ok: null
-		};
+		form.status = null;
+		form.message = null;
 	};
 
 	const handleInvalid: FormEventHandler<HTMLInputElement> = (e) => {
 		e.preventDefault();
 		if (e.currentTarget.validity.valueMissing) {
-			form.status = {
-				ok: false,
-				msg: 'لطفا کد تخفیف را وارد کنید!'
-			};
+			form.status = 'error';
+			form.message = 'لطفا کد تخفیف را وارد کنید!';
 		}
 	};
 </script>
 
-<form
-	class="discount-code {form.status.ok === false ? 'error' : ''} {form.status.ok === true
-		? 'success'
-		: ''} {form.isSubmitting ? 'submit' : ''}"
-	onsubmit={handleSubmit}
->
+<form class="discount-code {form.isSubmitting ? 'submit' : ''}" onsubmit={handleSubmit}>
 	<div class="wrapper">
 		<SwitchBox label="کد تخفیف">
 			<SwitchBoxInput
 				placeholder="کد تخفیف خود را وارد کنید..."
+				title="کد تخفیف"
 				oninput={handleInput}
 				oninvalid={handleInvalid}
 				required
 			/>
 			<Button type="submit" class="discount-code-button">ثبت</Button>
-			<span class="message"><span class="text">{form.status.msg}</span></span>
+			<Status type={form.status} message={form.message} />
 		</SwitchBox>
 	</div>
 </form>
@@ -87,36 +74,5 @@
 	.discount-code.submit .wrapper {
 		opacity: 0.5;
 		pointer-events: none;
-	}
-
-	.message {
-		display: flex;
-		align-items: end;
-		justify-content: center;
-		transition-property: height, transform, opacity;
-		transition-duration: var(--duration);
-		overflow: hidden;
-		height: 0;
-		opacity: 0;
-		transform: translateY(-10);
-	}
-
-	.discount-code.error .message {
-		color: var(--error);
-	}
-
-	.discount-code.success .message {
-		color: var(--success);
-	}
-
-	.discount-code.error .message,
-	.discount-code.success .message {
-		height: 3.6rem;
-		transform: translateY(0);
-		opacity: 1;
-	}
-
-	.text {
-		height: 1em;
 	}
 </style>
--- src/routes/products/[id]/_lib/component/comments.svelte
+++ src/routes/products/[id]/_lib/component/comments.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
 	import Button from '$lib/component/button.svelte';
 	import Comment from '$lib/component/comment.svelte';
-	import type { FormEventHandler } from 'svelte/elements';
+	import type { FormEventHandler, EventHandler } from 'svelte/elements';
 	import { singleProduct } from '../data/single-product';
+	import Status from '$lib/component/status.svelte';
+
+	type Form = {
+		status: 'error' | 'success' | null;
+		message: string | null;
+		isSubmitting: boolean;
+	};
+	const form: Form = $state({
+		status: null,
+		message: null,
+		isSubmitting: false
+	});
 
 	const IS_USER_LOGGED_IN = true;
 
 	const { commentList } = singleProduct;
 
-	const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
 		e.preventDefault();
+		form.status = null;
+		form.message = null;
+		form.isSubmitting = true;
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		form.isSubmitting = false;
+		form.status = 'success';
+		form.message = 'دیدگاه شما با موفقیت ارسال شد!';
+	};
+
+	const handleInvalid: FormEventHandler<HTMLTextAreaElement> = (e) => {
+		e.preventDefault();
+		if (e.currentTarget.validity.valueMissing) {
+			form.status = 'error';
+			form.message = 'متن دیدگاه الزامیست!';
+		}
+	};
+
+	const handleInput = () => {
+		form.status = null;
+		form.message = null;
 	};
 </script>
 
-<section class="comments {!commentList.length ? 'no-comment' : ''}">
+<section
+	class="comments {!commentList.length ? 'no-comment' : ''} {form.isSubmitting ? 'submitting' : ''}"
+>
 	<h2 class="title">دیدگاه مشتریان</h2>
 	<div class="content">
 		{#if commentList.length > 0}
@@ -27,8 +61,18 @@
 		{/if}
 		{#if IS_USER_LOGGED_IN}
 			<form class="comment-form box rounded-lg" onsubmit={handleSubmit}>
-				<textarea class="text" placeholder="دیدگاه خود را اینجا بنویسید..."></textarea>
-				<Button type="submit" class="submit">ثبت دیدگاه</Button>
+				<div class="comment-form-wrapper">
+					<textarea
+						class="text"
+						placeholder="دیدگاه خود را اینجا بنویسید..."
+						title="متن دیدگاه"
+						required
+						oninvalid={handleInvalid}
+						oninput={handleInput}
+					></textarea>
+					<Button type="submit" class="submit">ثبت دیدگاه</Button>
+					<Status type={form.status} message={form.message} />
+				</div>
 			</form>
 		{/if}
 	</div>
@@ -53,15 +97,23 @@
 	}
 
 	.comment-form {
-		display: flex;
-		flex-direction: column;
-		gap: 1.6rem;
 		padding: 2rem;
 		position: sticky;
 		top: 2rem;
 		min-width: 32rem;
-		height: 28rem;
-		flex-shrink: 0;
+		min-height: 28rem;
+		align-self: flex-start;
+	}
+
+	.comment-form-wrapper {
+		display: flex;
+		flex-direction: column;
+		transition: opacity var(--duration);
+	}
+
+	.comments.submitting .comment-form-wrapper {
+		opacity: 0.5;
+		pointer-events: none;
 	}
 
 	.no-comment .comment-form {
@@ -72,6 +124,7 @@
 	}
 
 	.comment-form :global(.submit) {
+		margin-top: 2rem;
 		width: 100%;
 	}
 
@@ -84,6 +137,7 @@
 		background: transparent;
 		border: 0;
 		outline: 0;
+		height: 17rem;
 	}
 
 	@media (max-width: 48rem) {
@@ -95,6 +149,7 @@
 			position: relative;
 			order: 1;
 			top: 0;
+			width: 100%;
 		}
 
 		.list {
